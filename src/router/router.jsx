8470fcd1ff03219a2dbfa5b0e2b8895a4607995b
Rahul mallik <rmallik191242@bscse.uiu.ac.bd>
2025-05-07 01:49:36 -0700

    up

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,8 +1,10 @@
 
 import {createBrowserRouter} from 'react-router'
+import App from '../App'
+import Root from '../pages/Root'
 
 
 export  const rootRouter=   createBrowserRouter([
-    {path: '/', element: <h1>hello i am home page</h1>},
+    {path: '/', element: <Root/>},
     {path: '/about', element: <h1>hello i am about page</h1>},
 ])
