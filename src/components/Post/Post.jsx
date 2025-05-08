4438e11f4cde59617f962e64cfd49735f31dc5b4
Rahul mallik <rmallik191242@bscse.uiu.ac.bd>
2025-05-08 14:13:58 +0600

    usenavigate aded

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
-import { Link } from 'react-router';
+import { Link, useNavigate } from 'react-router';
 
 export const Post = ({post}) => {
     const { id, title } = post; 
+    const Navigate= useNavigate();
+    const handleShowDetails = () => {
+        Navigate(`/post/${id}`)
+    }
     return (
         <div>
             <h4>Post of ID: {id}</h4>
             <p>{title}</p>
             <Link to={`/post/${id}`}>Post Detail</Link>
+            <button onClick={handleShowDetails}>
+                Click to see post details
+            </button>
             
         </div>
     )
